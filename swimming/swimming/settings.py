26f3c6c636c5f95a8b1fe132e41819cad24f5e0d
maxpurrp <seminm041@gmail.com>
2023-07-10 17:02:44 +0300

    add docker-compose

--- swimming/swimming/settings.py
+++ swimming/swimming/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1mj@6)4c)7c1zhrdnioqa*zqqcime=*b=cp37x^yh((%@u^8=m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
